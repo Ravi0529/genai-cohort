6453d9c3490cb2a29b5bec5064c24585d48b5ab2
Ravi0529 <150603739+Ravi0529@users.noreply.github.com>
2025-04-08 19:31:46 +0530

    class-work

--- Lecture-0-Intro-to-AI/class-work/tokenization.py
+++ Lecture-0-Intro-to-AI/class-work/tokenization.py
@@ -1,10 +1,12 @@
 import tiktoken
 
-encoder = tiktoken.get_encoding("gpt-4o")
+encoder = tiktoken.encoding_for_model("gpt-4o")
 
 print("Vocab size: ", encoder.n_vocab)
 
 text = "This is a test sentence. Let's see how it gets tokenized!"
 tokens = encoder.encode(text)
 
-print("Tokens: ", tokens) # random tokens: [444, 554, 6775, 34554, 355]
+print("Tokens: ", tokens) # [2500, 382, 261, 1746, 21872, 13, 41021, 1921, 1495, 480, 8625, 6602, 2110, 0]
+
+print("Decoded: ", encoder.decode(tokens)) # This is a test sentence. Let's see how it gets tokenized!
