54e55c2655681668c505697075ad3e729705937b
Ravi0529 <150603739+Ravi0529@users.noreply.github.com>
2025-05-28 01:59:42 +0530

    main-update(no-checkpointing)

--- Lecture-10-LangGraph(Checkpointing)/class-work/app/main.py
+++ Lecture-10-LangGraph(Checkpointing)/class-work/app/main.py
@@ -1,6 +1,29 @@
-def main():
-    print("Hello from app!")
+from .graph import graph
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
-if __name__ == "__main__":
-    main()
+def init():
+    while True:
+        user_input = input("> ")
+        if user_input.lower() == "clear":
+            break
+        # graph.invoke() vs graph.stream()
+        # invoke ---> returns the final result after processing all messages(all nodes)
+        # stream ---> returns a result after every node is processed
+
+        # result = graph.invoke({"messages": [{"role": "user", "content": user_input}]})
+        # print(result)
+
+        for event in graph.stream(
+            {"messages": [{"role": "user", "content": user_input}]},
+            stream_mode="values",
+        ):
+            if "messages" in event:
+                event["messages"][
+                    -1
+                ].pretty_print()  # issue here is ---> no previous messages are stored by the graph, everytime the user asks a question, it fresh starts the graph (i.e. no checkpointing)
+
+
+init()
