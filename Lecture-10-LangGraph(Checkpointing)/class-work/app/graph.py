7eb9efa27621ecf3ae93602f6f6b2d14965f659b
Ravi0529 <150603739+Ravi0529@users.noreply.github.com>
2025-05-28 01:37:20 +0530

    graph-phase-1

---
+++ Lecture-10-LangGraph(Checkpointing)/class-work/app/graph.py
@@ -0,0 +1,32 @@
+from typing import Annotated
+from typing_extensions import TypedDict
+from langgraph.graph.message import add_messages
+from lanchain.chat_models import init_chat_model
+from langgraph.graph import StateGraph, START, END
+
+llm = init_chat_model(
+    model_provider="openai", model="gpt-4o-mini"
+)  # Initialize the chat model using OpenAI's GPT-3.5 Turbo (using langchain --> going more abstract)
+
+
+class State(TypedDict):
+    messages: Annotated[
+        list, add_messages
+    ]  # Annotated type with metadata (type: list, metadata: add_messages)
+
+
+def chatbot(state: State):
+    return {
+        "messages": [llm.invoke[state["messages"]]]
+    }  # llm invokes all the messages in the state
+
+
+graph_builder = StateGraph(State)
+
+### Nodes and Edges
+graph_builder.add_node("chatbot", chatbot)
+
+graph_builder.add_edge(START, "chatbot")
+graph_builder.add_edge("chatbot", END)
+
+graph = graph_builder.compile()
